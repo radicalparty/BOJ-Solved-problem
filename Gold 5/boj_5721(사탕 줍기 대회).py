a7eaf38c9ec7e96a786e5589799bae9d2b4c1ab4
radicalparty <85662273+radicalparty@users.noreply.github.com>
2021-06-30 13:42:56 +0900

    Create boj_5721(사탕 줍기 대회).py

---
+++ Gold 5/boj_5721(사탕 줍기 대회).py
@@ -0,0 +1,27 @@
+#dp문제인데 좀 어이 없이 푼 문제
+#예시로 중간을 처음 선택한다는 점에서 좀 혼란이 오다가 중간 줄부터 먼저 선택하든 첫 번째 세로줄 부터 선택하든 상관이 없다는 것을 깨달음
+#어쨌든 며칠 동안 고민해봐서 재밌었던 문제
+import sys
+while True:
+    n, m = map(int, sys.stdin.readline().split())
+    if n == m == 0:
+        break
+    mpg = [[0 for _ in range(m + 1)]] + [[0] + list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+    maxcandy = [0 for _ in range(n + 1)]
+    ans = -float('inf')
+    for i in range(1, n + 1):
+        dp = [0 for _ in range(m + 1)]
+        plus = -float('inf')
+        for j in range(1, m + 1):
+            if j < 3:
+                dp[j] = mpg[i][j]
+            else:
+                dp[j] = max(dp[j - 2], dp[j - 3]) + mpg[i][j]
+            plus = max(plus, dp[j])
+        if i < 3:
+            maxcandy[i] = plus
+            ans = max(ans, maxcandy[i])
+        else:
+            maxcandy[i] = max(maxcandy[i - 2], maxcandy[i - 3]) + plus
+            ans = max(ans, maxcandy[i])
+    print(ans)
